2bb48bf34bc244f855f2741e57377e07dd82d3ec
VVeremjova <vveremjova@luxoft.com>
2015-12-12 21:23:14 +0200

    Finished with updating user info

--- admin/AdminVisual.py
+++ admin/AdminVisual.py
@@ -7,7 +7,7 @@ import ConfigParser
 
 class  AdminVisualPart(Tkinter.Tk):
     """docstring for  AdminVisualPart"""
-    def __init__(self, parent,user_name):
+    def __init__(self, parent,user_name,result_searching=None):
         Tkinter.Tk.__init__(self,parent)
         self.parent = parent
 
@@ -15,6 +15,8 @@ class  AdminVisualPart(Tkinter.Tk):
         config.read('format.ini')
         self.db_name = config.get('Main', 'db_name')
         self.showFields(user_name)
+        if result_searching != None:
+            self.updateFields(result_searching)
 
     def showFields(self,user_name=""):
 
@@ -31,13 +33,6 @@ class  AdminVisualPart(Tkinter.Tk):
 
         self.lblsex = Tkinter.Label(self, text = "Gender:")
         self.lblsex.pack()
-        # self.checkSex = Tkinter.IntVar()
-        # R1 = Tkinter.Radiobutton(self, text="Male", variable=self.checkSex, value=1,
-        #           command=self.selectedSex)
-        # R2 = Tkinter.Radiobutton(self, text="Female", variable=self.checkSex, value=2,
-        #           command=self.selectedSex)
-        # R1.pack( anchor = Tkinter.W )
-        # R2.pack( anchor = Tkinter.W )
         self.entry_sex = Tkinter.Entry(self)
         self.entry_sex.pack()
 
@@ -48,19 +43,16 @@ class  AdminVisualPart(Tkinter.Tk):
         self.entry_birthday.pack()
         self.lbl_occupation = Tkinter.Label(self, text = "Occupation:")
         self.lbl_occupation.pack()
-
         self.entry_occupation = Tkinter.Entry(self)
         self.entry_occupation.pack()
-        # self.entry1.grid(column=1,row=1,columnspan=1)
 
         self.lbl_expirience = Tkinter.Label(self, text = "Overall experience (years):")
         self.lbl_expirience.pack()
-
         self.entry_expirience = Tkinter.Entry(self)
         self.entry_expirience.pack()
+
         self.lbl_lastexp = Tkinter.Label(self, text = "Work experience in the last positions (years):")
         self.lbl_lastexp.pack()
-
         self.entry_lastexp = Tkinter.Entry(self)
         self.entry_lastexp.pack()
 
@@ -73,36 +65,45 @@ class  AdminVisualPart(Tkinter.Tk):
         BSave = Tkinter.Button(self, text ="Save", command = self.save)
         BSave.pack(side = Tkinter.RIGHT)
 
-        BUpdate = Tkinter.Button(self, text ="Update", command = self.updateFields)
+        BUpdate = Tkinter.Button(self, text ="Update", command = self.update)
         BUpdate.pack(side = Tkinter.LEFT)
 
     def save(self):
-        # tkMessageBox.showinfo( "Demo", "Will save new user")
         db = SaveInDB(self.db_name)
+        data = self.prepareDataToSave(False)
+        db.addNewClient(data)        
+        db.close()
+
+    def update(self):
+        db = SaveInDB(self.db_name)   
+        data = self.prepareDataToSave(True) 
+        db.updateClient(data)
+        db.close()
+
+    def prepareDataToSave(self, update = False):
         name =  self.entry_name.get()
         id_ = self.entry_id.get()
 
         gender = self.entry_sex.get()
-        date_age = self.entry_id.get()
-        occupation = self.entry_id.get()
-        work_experience = self.entry_id.get()
-        last_work_experience = self.entry_id.get()
-        comments = self.entry_id.get()
-        # db.addNewClient()
-      
-        db.addNewClient(id_, name, gender, date_age, occupation,work_experience,last_work_experience,comments)
-        db.close()
+        date_age = self.entry_birthday.get()
+        occupation = self.entry_occupation.get()
+        work_experience = self.entry_expirience.get()
+        last_work_experience = self.entry_lastexp.get()
+        comments = self.entry_comment.get()
+        if update == False:
+            return (id_, name, gender, date_age, occupation,work_experience,last_work_experience,comments)
+        else:
+            return (id_, gender, date_age, occupation,work_experience,last_work_experience,comments, name)
+  
+    def updateFields(self,list_with_entries):
+        self.entry_id.insert(0,list_with_entries[0])
+        self.entry_sex.insert(0,list_with_entries[2])
+        self.entry_birthday.insert(0,list_with_entries[3])
+        self.entry_occupation.insert(0,list_with_entries[4])
+        self.entry_expirience.insert(0,list_with_entries[5])
+        self.entry_lastexp.insert(0,list_with_entries[6])
+        self.entry_comment.insert(0,list_with_entries[7])
 
-    # def UpdateFields(self,user_name="",id_,gender,date_age,occupation,work_experience,last_work_experience,comments):
-    def updateFields(self,user_name,id_,gender,date_age,occupation,work_experience,last_work_experience,comments):
-        pass
-        # db = SaveInDB(self.db_name)
-        # values = db.searchClient(user_name)
-        # if values==None :
-        #     tkMessageBox.showinfo( "Error", "User not found")
-        # else:
-        #     print values
-        # db.close()
 
 # var = IntVar()
 # R1 = Radiobutton(root, text="Option 1", variable=var, value=1,
--- admin/saveInDB.py
+++ admin/saveInDB.py
@@ -14,31 +14,31 @@ class SaveInDB() :
                       occupation text, work_experience text,
                       last_work_experience text,
                       comments text)''')
+
+        # c.execute('''CREATE TABLE Results
+        #              (name text, total_errors text, gender text, date_age text, 
+        #               occupation text, work_experience text,
+        #               last_work_experience text,
+        #               comments text)''')
         self.conn.commit()
 
-    def addNewClient(self, id_, name, gender, date_age, occupation,work_experience,last_work_experience,comments ):
-        # Insert a row of data
-        params = (id_,name, gender, date_age, occupation,work_experience ,last_work_experience , comments)
+    def addNewClient(self,params):
         c = self.conn.cursor()
-    	c.execute("INSERT INTO Users VALUES (?,?, ?, ?, ?,? ,? , ?)",params)
-    	# Save (commit) the changes
-    	self.conn.commit()
+        c.execute("INSERT INTO Users VALUES (?,?, ?, ?, ?,? ,? , ?)",params)
+        # Save (commit) the changes
+        self.conn.commit()
 
     def searchClient(self, user_name):
         params = (user_name,)
         c = self.conn.cursor()        
         curs = c.execute('SELECT * FROM Users WHERE name=?', [user_name])
         res=c.fetchone()
-        print "==="
-        print res
         return res
 
-    def updateClient(self, id_, name, gender, date_age, occupation,work_experience,last_work_experience,comments ):
+    def updateClient(self,params):
         # Insert a row of data
-        params = (id_, gender, date_age, occupation,work_experience ,last_work_experience , comments,name)
         c = self.conn.cursor()
-        c.execute("UPDATE COMPANY SET id_ = ?, gender = ?,date_age = ?,occupation =?,work_experience=?,last_work_experience=?, comments = ?    WHERE name = ?;",params)
-        # Save (commit) the changes
+        c.execute("UPDATE Users SET id_ = ?, gender = ?,date_age = ?,occupation =?,work_experience=?,last_work_experience=?, comments = ?    WHERE name = ?;",params)
         self.conn.commit()
 
     def close(self):
